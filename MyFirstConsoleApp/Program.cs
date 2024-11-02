c047676caf87eb5352ad7715daaeaee9c583086d
NateTheUnavailable <nate@stevenogardner.com>
2024-11-01 18:03:13 -0600

    working thing sorta

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string contents = File.ReadAllText(@"C:\");
+string choice = "";
+choice = Console.ReadLine();
+choice = choice.ToUpper();
+if(choice == "A")
+{
+    string contents = File.ReadAllText(@"C:\story1.txt");
+    Console.WriteLine(choice);
+}
+if(choice == "B")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "C")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "D")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "E")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "F")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "G")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "H")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "I")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "J")
+{
+    Console.WriteLine(choice);
+}
+if(choice == "K")
+{
+    Console.WriteLine(choice);
+}
+else{
+    Console.WriteLine("thanks for playing my game");
+}
+Console.WriteLine(choice);
